8613dcce4213ae224ab05cbd2268b98562e70b5b
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-11-26 16:18:10 -0300

    arquivo js

---
+++ mod1_fundamentos/bloco4_javascript/dia1/scripts.js
@@ -0,0 +1,16 @@
+let situacao;
+
+situacao="passaro"
+switch(situacao){
+    case "aprovada":
+        console.log("aprovada");
+        break;
+    case "lista":
+        console.log("lista");
+        break;
+    case "rerovada":
+        console.log("rerovada");
+        break;
+    default:
+        console.log("situação não se aplica");
+}
