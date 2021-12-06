2d5867a16df7cf007632fa97c3d9a2b14a0c0a4e
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-06 16:24:32 -0300

    ajustes

---
+++ mod1_fundamentos/bloco4_javascript/dia4/scripts.js
@@ -0,0 +1,37 @@
+// EXERCICIO 1 DE FUNCOES 4.4
+// function palindromo(nome){
+//     let nomeRev='';
+//     let resultado = true;
+//     tam=nome.length;
+//     nomeRev=nome.split('')
+//     nomeRev=nomeRev.reverse()
+//     nome=nome.split('')
+//     for (i=0;i<tam;i+=1){
+//         if (nome[i]!=nomeRev[i]){
+//             resultado = false
+//             return resultado
+//             break
+//         } 
+//     }
+//     return resultado    
+// }
+
+// resultado = palindromo('porto');
+// console.log(resultado)
+
+
+// EXERCICIO 2 DE FUNCOES 4.4
+// function indiceMaior(arr){
+// maior=arr[0];
+// indice=0;
+// tam=arr.length;
+// for (i=0;i<tam;i+=1){
+//     if (arr[i]>maior){
+//         maior=arr[i];
+//         indice=i;
+//     }
+// } 
+// return indice
+// }
+// arr=[2,3,6,7,10,1]
+// console.log(indiceMaior(arr))
